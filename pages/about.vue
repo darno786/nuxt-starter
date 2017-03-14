a102305571c1eb7fed1d4fd03adca2e9e99347e3
darno786 <adnan_s14@outlook.com>
2017-03-14 21:09:41 +0000

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,7 @@
       This page is loaded from the {{ name }}
     </h1>
     <h2 class="info" v-if="name === 'client'">
-      Please refresh the page
+      For Enquiries Email :Contact@adnan-s.co.uk
     </h2>
     <nuxt-link class="button" to="/">
       Home page
