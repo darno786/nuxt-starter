0a94dc49969d9d7fa8348b92a2447f61bf60ce0f
darno786 <adnan_s14@outlook.com>
2017-03-14 18:40:47 +0000

    Add files via upload

---
+++ pages/contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <h1 class="title">
+      A Project By Adnan Shahnawaz
+    </h1>
+    <nuxt-link class="button" to="/about">
+      About page
+    </nuxt-link>
+    <nuxt-link class="button" to="/about">
+      Contact us
+    </nuxt-link>
+  </section>
+</template>
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
